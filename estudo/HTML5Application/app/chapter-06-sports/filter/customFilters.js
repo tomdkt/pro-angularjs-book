68e4795219ba6eb4d11776dfa9f230f33d771ec6
Thomas D. K. Teixeira <thomas.teixeira@b2wdigital.com>
2017-01-24 18:13:05 -0200

    pendente correcao url routing

--- estudo/HTML5Application/app/chapter-06-sports/filter/customFilters.js
+++ estudo/HTML5Application/app/chapter-06-sports/filter/customFilters.js
@@ -41,7 +41,7 @@ angular.module("customFilters", [])
       return function(data,size){
         if(angular.isArray(data)){
           var result = [];
-          for (var i = 0; i < Match.ceil(data.length / size); i++) {
+          for (var i = 0; i < Math.ceil(data.length / size); i++) {
             result.push(i);
           }
           return result;
