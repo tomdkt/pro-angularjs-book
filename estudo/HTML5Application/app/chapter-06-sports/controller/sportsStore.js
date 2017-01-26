986797fcd2a44b7263d2e7b28f844708c92e6417
Thomas D. K. Teixeira <thomas.teixeira@b2wdigital.com>
2017-01-26 17:05:48 -0200

    chapter 8 - parou| Defining the Authentication View

--- estudo/HTML5Application/app/chapter-06-sports/controller/sportsStore.js
+++ estudo/HTML5Application/app/chapter-06-sports/controller/sportsStore.js
@@ -6,13 +6,29 @@
 
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:2403/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("orderUrl", "http://localhost:2403/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
       $scope.data = {};
 
       $http.get(dataUrl).success(function (data) {
         $scope.data.products = data;
-      }).error(function(error, status) {
-        $scope.data.error = { message: error, status: status};
+      }).error(function (error, status) {
+        $scope.data.error = {message: error, status: status};
         console.log($scope.data.error.status);
       });
+
+      $scope.sendOrder = function (shippingDetails) {
+        var order = angular.copy(shippingDetails);
+        order.products = cart.getProducts();
+        $http.post(orderUrl, order)
+            .success(function (data) {
+              $scope.data.orderId = data.id;
+              cart.getProducts().length = 0;
+            })
+            .error(function (message, status) {
+              $scope.data.orderError = {message: message, status: status};
+            }).finally(function () {
+          $location.path("/complete");
+        });
+      }
     });
